c9e331f2aaae7d27a7903d56a3837d93fd558ba7
Aditya1406incom <aditya_hermansyahputra@yahoo.com>
2020-12-23 13:29:00 +0700

    splash

--- keluarga_bintoro/lib/main.dart
+++ keluarga_bintoro/lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-// import 'package:flutter_spinkit/flutter_spinkit.dart';
-// import 'package:shared_preferences/shared_preferences.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 import 'ui/pages/pages.dart';
 
@@ -20,49 +20,49 @@ class MyApp extends StatelessWidget {
   }
 }
 
-// class SplashPage extends StatefulWidget {
-//   @override
-//   _SplashPageState createState() => _SplashPageState();
-// }
+class SplashPage extends StatefulWidget {
+  @override
+  _SplashPageState createState() => _SplashPageState();
+}
 
-// class _SplashPageState extends State<SplashPage> {
-//   @override
-//   void initState() {
-//     super.initState();
-//     Future.delayed(Duration(seconds: 3), () {
-//       checkLoginStatus();
-//     });
-//   }
+class _SplashPageState extends State<SplashPage> {
+  @override
+  void initState() {
+    super.initState();
+    Future.delayed(Duration(seconds: 3), () {
+      checkLoginStatus();
+    });
+  }
 
-//   checkLoginStatus() async {
-//     final SharedPreferences sharedPreferences =
-//         await SharedPreferences.getInstance();
-//     final String token =
-//         sharedPreferences.getString("token"); //change with access_token
-//     print(token);
-//     if (token == null || token.isEmpty) {
-//       Navigator.push(context, MaterialPageRoute(builder: (_) => LandingPage()));
-//     } else {
-//       Navigator.of(context).pushAndRemoveUntil(
-//           MaterialPageRoute(builder: (BuildContext context) => HomePage()),
-//           (Route<dynamic> route) => false);
-//     }
-//   }
+  checkLoginStatus() async {
+    final SharedPreferences sharedPreferences =
+        await SharedPreferences.getInstance();
+    final String token =
+        sharedPreferences.getString("token"); //change with access_token
+    print(token);
+    if (token == null || token.isEmpty) {
+      Navigator.push(context, MaterialPageRoute(builder: (_) => LandingPage()));
+    } else {
+      Navigator.of(context).pushAndRemoveUntil(
+          MaterialPageRoute(builder: (BuildContext context) => HomePage()),
+          (Route<dynamic> route) => false);
+    }
+  }
 
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       backgroundColor: Colors.red[900],
-//       body: Column(
-//         mainAxisAlignment: MainAxisAlignment.center,
-//         crossAxisAlignment: CrossAxisAlignment.center,
-//         children: [
-//           // FlutterLogo(
-//           //   size: 80,
-//           // ),
-//           SpinKitThreeBounce(color: Colors.white, size: 24)
-//         ],
-//       ),
-//     );
-//   }
-// }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.red[900],
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          // FlutterLogo(
+          //   size: 80,
+          // ),
+          SpinKitThreeBounce(color: Colors.white, size: 24)
+        ],
+      ),
+    );
+  }
+}
